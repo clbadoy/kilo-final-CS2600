d974390d414ec4912c5541c22732cc560e946b8b
Christian Badoy <clbadoy@cpp.edu>
2021-12-08 04:51:22 -0800

    Added lsh_launch

--- shell.c
+++ shell.c
@@ -101,6 +101,35 @@ char **lsh_split_line(char *line)
     return tokens;
 }
 
+int lsh_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if(pid == 0)
+    {
+        if(execvp(args[0], args) == -1)
+        {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if(pid < 0)
+    {
+        perror("lsh");
+    }
+    else
+    {
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
 int main(int argc, char **argv)
 {
     lsh_loop();
