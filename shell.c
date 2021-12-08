3ea346b3fa31056b8697ed887c6e54409c6e295b
Christian Badoy <clbadoy@cpp.edu>
2021-12-08 03:31:54 -0800

    Initial commit

---
+++ shell.c
@@ -0,0 +1,13 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+    lsh_loop();
+
+    return EXIT_SUCCESS;
+}
