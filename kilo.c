05f789a507082d9b885c2e10c9d857b438a64412
Christian Badoy <clbadoy@cpp.edu>
2021-12-08 06:29:44 -0800

    Added disableRawMode

--- kilo.c
+++ kilo.c
@@ -1,8 +1,17 @@
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    
+    struct termios raw = orig_termios;
 
     tcgetattr(STDIN_FILENO, &raw);
 
