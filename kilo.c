de5c8cc16729fcd778dfd434f27d9a261070e599
Christian Badoy <clbadoy@cpp.edu>
2021-12-10 07:36:21 -0800

    5: Implemented Incremential Search

--- kilo.c
+++ kilo.c
@@ -68,6 +68,7 @@ struct editorConfig E;
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
 char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** terminal ***/
 
@@ -384,7 +385,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
         if (E.filename == NULL) {
             editorSetStatusMessage("Save aborted");
             return;
@@ -413,9 +414,10 @@ void editorSave() {
 
 /*** find ***/
 
-void editorFind() {
-    char *query = editorPrompt("Search: %s (ESC to cancel)");
-    if (query == NULL) return;
+void editorFindCallback(char *query, int key) {
+    if (key == '\r' || key == '\x1b') {
+        return;
+    }
     int i;
     for (i = 0; i < E.numrows; i++) {
         erow *row = &E.row[i];
@@ -427,7 +429,13 @@ void editorFind() {
             break;
         }
     }
-    free(query);
+}
+
+void editorFind() {
+    char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+    if (query) {
+        free(query);
+    }
 }
 
 /*** append buffer ***/
@@ -574,7 +582,7 @@ void editorSetStatusMessage(const char *fmt, ...) {
 
 /*** input ***/
 
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
     size_t bufsize = 128;
     char *buf = malloc(bufsize);
     size_t buflen = 0;
@@ -589,12 +597,16 @@ char *editorPrompt(char *prompt) {
         }
         else if (c == '\x1b') {
             editorSetStatusMessage("");
+            if (callback) 
+                callback(buf, c);
             free(buf);
             return NULL;
         }
         else if (c == '\r') {
             if (buflen != 0) {
                 editorSetStatusMessage("");
+                if (callback) 
+                    callback(buf, c);
                 return buf;
             }
         } 
@@ -606,6 +618,8 @@ char *editorPrompt(char *prompt) {
             buf[buflen++] = c;
             buf[buflen] = '\0';
         }
+        if (callback)
+            callback(buf, c);
     }
 }
 
