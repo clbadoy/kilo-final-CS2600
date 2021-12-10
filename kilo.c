c5ff9437dd3de06d91cbb9994f943e71c4d459d6
Christian Badoy <clbadoy@cpp.edu>
2021-12-10 10:37:51 -0800

    6: Implement Colored keywords

--- kilo.c
+++ kilo.c
@@ -40,6 +40,8 @@ enum editorKey {
 enum editorHighlight {
     HL_NORMAL = 0,
     HL_COMMENT,
+    HL_KEYWORD1,
+    HL_KEYWORD2,
     HL_STRING,
     HL_NUMBER,
     HL_MATCH
@@ -51,6 +53,7 @@ enum editorHighlight {
 struct editorSyntax {
     char *filetype;
     char **filematch;
+    char **keywords;
     char *singleline_comment_start;
     int flags;
 };
@@ -85,11 +88,18 @@ struct editorConfig E;
 /*** filetypes ***/
 
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
+char *C_HL_keywords[] = {
+    "switch", "if", "while", "for", "break", "continue", "return", "else",
+    "struct", "union", "typedef", "static", "enum", "class", "case",
+    "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+    "void|", NULL
+};
 
 struct editorSyntax HLDB[] = {
     {
         "c",
         C_HL_extensions,
+        C_HL_keywords,
         "//",
         HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
     },
@@ -240,6 +250,8 @@ void editorUpdateSyntax(erow *row) {
     
     if (E.syntax == NULL) return;
 
+    char **keywords = E.syntax->keywords;
+
     char *scs = E.syntax->singleline_comment_start;
     int scs_len = scs ? strlen(scs) : 0;
 
@@ -257,7 +269,7 @@ void editorUpdateSyntax(erow *row) {
                 break;
             }
         }
-        
+
         if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
             if (in_string) {
                 row->hl[i] = HL_STRING;
@@ -286,7 +298,26 @@ void editorUpdateSyntax(erow *row) {
                 continue;
             }
         }
-
+        
+        if (prev_sep) {
+            int j;
+            for (j = 0; keywords[j]; j++) {
+                int klen = strlen(keywords[j]);
+                int kw2 = keywords[j][klen - 1] == '|';
+                if (kw2) klen--;
+                    if (!strncmp(&row->render[i], keywords[j], klen) &&
+                                    is_separator(row->render[i + klen])) {
+                        memset(&row->hl[i], kw2 ? HL_KEYWORD2 : HL_KEYWORD1, klen);
+                        i += klen;
+                        break;
+                    }
+            }
+            if (keywords[j] != NULL) {
+                prev_sep = 0;
+                continue;
+            }
+        }
+        
     prev_sep = is_separator(c);
     i++;
     }
@@ -296,6 +327,10 @@ int editorSyntaxToColor(int hl) {
     switch (hl) {
         case HL_COMMENT: 
             return 36;
+        case HL_KEYWORD1: 
+            return 33;
+        case HL_KEYWORD2:
+            return 32;
         case HL_STRING: 
             return 35;
         case HL_NUMBER:
